55ebcc11fdf1c6e633ce05643f4843534af756c7
mr128254 <moritz.reh@gmail.com>
2024-02-27 20:08:45 +0100

    remove animate test

--- test/visualization_test.py
+++ test/visualization_test.py
@@ -14,16 +14,16 @@ with open(__file__.rsplit("/", 1)[0] + "/data.pickle", "rb") as f:
 
 def test_visualization():
     os.makedirs(cfg["utilParameters"]["path"])
-    jTWA.visualization.create_visuals(obs, cfg)
+    jTWA.visualization.plot_deviations(obs, cfg)
+    jTWA.visualization.plot_spin_obs(obs, cfg)
+    jTWA.visualization.plot_correlation_matrices(obs, cfg)
 
     assert os.path.isfile(cfg["utilParameters"]["path"] + "deviations.pdf")
     assert os.path.isfile(cfg["utilParameters"]["path"] + "spin_obs.pdf")
     assert os.path.isfile(cfg["utilParameters"]["path"] + "corrcoefs.pdf")
-    assert os.path.isfile(cfg["utilParameters"]["path"] + "evol.mp4")
 
     os.remove(cfg["utilParameters"]["path"] + "deviations.pdf")
     os.remove(cfg["utilParameters"]["path"] + "spin_obs.pdf")
     os.remove(cfg["utilParameters"]["path"] + "corrcoefs.pdf")
-    os.remove(cfg["utilParameters"]["path"] + "evol.mp4")
     os.rmdir(cfg["utilParameters"]["path"])
     os.rmdir(cfg["utilParameters"]["path"].rsplit("/", 2)[0] + "/")
