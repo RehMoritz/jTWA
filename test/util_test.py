5b48910a9d2431a130cda958afe0d0cd6c38a15d
mr128254 <moritz.reh@gmail.com>
2024-02-27 10:41:37 +0100

    add tests for reading and writing data.

---
+++ test/util_test.py
@@ -0,0 +1,25 @@
+import os
+import json
+import pytest
+
+import jTWA
+
+
+with open("test_config.json") as f:
+    cfg = json.load(f)
+
+cfg = jTWA.spin1.hamiltonian.update_cfg(cfg)
+
+
+def test_store_data():
+    obs = {"myTestObservable": 1}
+
+    jTWA.util.store_data(obs, cfg)
+    read_obs = jTWA.util.read_data(cfg)
+
+    assert obs == read_obs
+
+    os.remove(cfg["utilParameters"]["path"] + "data.pickle")
+    os.remove(cfg["utilParameters"]["path"] + "config.json")
+    os.rmdir(cfg["utilParameters"]["path"])
+    os.rmdir(cfg["utilParameters"]["path"].split("/")[0] + "/")
