bdacd866aa7b2cc1b7bfa1f1e41ac42b6dd96d08
mr128254 <moritz.reh@gmail.com>
2024-02-28 14:24:43 +0100

    Update version number.

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ import jTWA
 project = "jTWA"
 copyright = "2024, Moritz Reh"
 author = "Moritz Reh"
-release = "0.0.3"
+release = "0.0.4"
 
 # -- General configuration ---------------------------------------------------
 
